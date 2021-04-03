ea6fef43d5f934461c29ceae3f6952cbad9518c5
Nicolaas0 <nicolerrick@gmail.com>
2021-04-04 01:06:24 +0700

    add toLogin method for button

--- app/src/main/java/com/example/e_wallet/Regist.java
+++ app/src/main/java/com/example/e_wallet/Regist.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class Regist extends AppCompatActivity {
 
+	private TextView tv_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
     }
+	
+	public void toLogin(View v){
+		Intent intent = new Intent(this,activity_login.class);
+		startActivity(intent);
 }
