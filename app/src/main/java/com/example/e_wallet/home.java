92909837cff33f8f1cb180bdbd7619a074f6e521
Nicolaas0 <nicolerrick@gmail.com>
2021-04-05 01:39:49 +0700

    add method for got to other pages

--- app/src/main/java/com/example/e_wallet/home.java
+++ app/src/main/java/com/example/e_wallet/home.java
@@ -11,4 +11,34 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+	
+	public void toHistory(View v){
+		Intent intent = new Intent(this,history.class);
+		startActivity(intent);
+	}
+	
+	public void toPromo(View v){
+		Intent intent = new Intent(this,promo.class);
+		startActivity(intent);
+	}
+	
+	public void toProfile(View v){
+		Intent intent = new Intent(this,promo.class);
+		startActivity(intent);
+	}
+	
+	public void toTransfer(View v){
+		Intent intent = new Intent(this,transfer.class);
+		startActivity(intent);
+	}
+	
+	public void toRequest(View v){
+		Intent intent = new Intent(this,request.class);
+		startActivity(intent);
+	}
+	
+	public void toTopup(View v){
+		Intent intent = new Intent(this,topup.class);
+		startActivity(intent);
+	}
 }
