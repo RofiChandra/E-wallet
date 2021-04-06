1b1139c06a76aca3584b93b33114c3accec1d140
Nicolaas0 <nicolerrick@gmail.com>
2021-04-06 23:26:46 +0700

    add dialog to exit button

--- app/src/main/java/com/example/e_wallet/profile.java
+++ app/src/main/java/com/example/e_wallet/profile.java
@@ -16,4 +16,23 @@ public class profile extends AppCompatActivity {
     //     Intent intent = new Intent(this,promo_code.class);
     //     startActivity(intent);
     // }
-}
+
+    public void exit(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Apakah anda yakin untuk logout?");
+        builder.setPositiveButton("Ya, Saya yakin.", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Tidak, saya tidak yakin.", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+    }
