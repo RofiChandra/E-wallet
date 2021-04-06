a9270f59aa8c58f16c7271246e7051e188d32f4c
ROFI_CHANDRA_SAPUTRA <rofi.chandra18@gmail.com>
2021-04-06 16:14:03 +0700

    Layout Promo

--- app/src/main/java/com/example/e_wallet/Regist.java
+++ app/src/main/java/com/example/e_wallet/Regist.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.text.TextUtils.isEmpty;
 
 public class Regist extends AppCompatActivity {
     private EditText et_email;
